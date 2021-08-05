cddf06e5e9b5717315c5a490bb76440681554491
David Sanchez <dsanva2004@gmail.com>
2021-08-05 17:03:14 -0500

    feat:create ProductCard component and temporary styles

---
+++ src/components/Main/Results/ProductCard/ProductCard.jsx
@@ -0,0 +1,17 @@
+//Dependencies
+import React from 'react'
+
+//Styles
+import './ProductCard.css'
+
+const ProductCard = () => {
+    return(
+        <>
+            <article className="ProductCard">
+                Product
+            </article>
+        </>
+    );
+}
+
+export default ProductCard;
