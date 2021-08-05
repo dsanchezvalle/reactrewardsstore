8940165d0f985db39e214df6237f71149391b01f
David Sanchez <dsanva2004@gmail.com>
2021-08-04 23:04:26 -0500

    feat:adjust selects Filters padding

--- src/components/Main/TopBar/Filters/Filters.jsx
+++ src/components/Main/TopBar/Filters/Filters.jsx
@@ -10,8 +10,8 @@ const Filters = () => {
             <p className="FilterLabel">Sort by:</p>
             <select className="FilterPrice" name="filter1" id="filter1" /* value={0} */>
                 <option value={0}>Price</option>
-                <option value={1}>Lowest Price</option>
-                <option value={2}>Highest Price</option>
+                <option value={1}>Lowest $</option>
+                <option value={2}>Highest $</option>
             </select>
             <select className="FilterCategory" name="FilterCategory" id="filter2" /* value={0} */>
                 <option value={0}>Category</option>
