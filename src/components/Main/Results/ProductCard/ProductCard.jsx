62a35701d102993dd58a8daa17faf66be26f1dd7
David Sanchez <dsanva2004@gmail.com>
2021-08-05 17:29:22 -0500

    feat:create temporary ProductCard structure to test it

--- src/components/Main/Results/ProductCard/ProductCard.jsx
+++ src/components/Main/Results/ProductCard/ProductCard.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 //Styles
 import './ProductCard.css'
 
-const ProductCard = () => {
+const ProductCard = ({name, category, imagePath}) => {
     return(
         <>
             <article className="ProductCard">
-                Product
+                <h3>{name}</h3>
+                <p>{category}</p>
+                <img src={imagePath} alt={name} />
             </article>
         </>
     );
