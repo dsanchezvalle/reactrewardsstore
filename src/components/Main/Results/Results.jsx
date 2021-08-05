8f67e5385be0aa6b8f51ee90624588c1f46d8342
David Sanchez <dsanva2004@gmail.com>
2021-08-05 17:03:53 -0500

    feat:test new ProductCard component inside Results component

--- src/components/Main/Results/Results.jsx
+++ src/components/Main/Results/Results.jsx
@@ -2,27 +2,18 @@
 import React from "react";
 //Styles
 import './Results.css'
+//Components
+import ProductCard from "./ProductCard/ProductCard";
 
 const Results = () => {
+    let products = [];
+    for(let i= 0; i<=3; i++){
+        products.push(<ProductCard/>);
+    }
     return(
         <>
         <section className="ResultsGrid">
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
-            <div className="ProductCard">Product</div>
+            {products}
         </section>
         </>
     );
