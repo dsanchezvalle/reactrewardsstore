fb5e9cc212f2df1dc05058de01b950e70c5e5d3e
David Sanchez <dsanva2004@gmail.com>
2021-08-04 19:39:50 -0500

    feat:add responsive styles for PaginationArrows and adjust Filters width

--- src/components/Main/TopBar/Filters/Filters.jsx
+++ src/components/Main/TopBar/Filters/Filters.jsx
@@ -13,7 +13,7 @@ const Filters = () => {
                 <option value={1}>Lowest Price</option>
                 <option value={2}>Highest Price</option>
             </select>
-            <select name="FilterCategory" id="filter2" /* value={0} */>
+            <select className="FilterCategory" name="FilterCategory" id="filter2" /* value={0} */>
                 <option value={0}>Category</option>
                 <option value={1}>Category 1</option>
                 <option value={2}>Category 2</option>
