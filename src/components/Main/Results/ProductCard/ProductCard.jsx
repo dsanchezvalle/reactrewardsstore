bdecffc594a06450d6a630fc6ba9cd557e041b73
David Sanchez <dsanva2004@gmail.com>
2021-08-07 13:07:17 -0500

    feat:create ProductCard main structure

--- src/components/Main/Results/ProductCard/ProductCard.jsx
+++ src/components/Main/Results/ProductCard/ProductCard.jsx
@@ -1,6 +1,5 @@
 //Dependencies
 import React from 'react'
-
 //Styles
 import './ProductCard.css'
 
@@ -8,9 +7,11 @@ const ProductCard = ({name, category, imagePath}) => {
     return(
         <>
             <article className="ProductCard">
-                <h3>{name}</h3>
-                <p>{category}</p>
-                <img src={imagePath} alt={name} />
+            <img className="ProductCard__Image" src={imagePath} alt={name} />
+            <section className="ProductCard__Info">
+                <p className="ProductCard__Category">{category}</p>
+                <h3 className="ProductCard__Name">{name}</h3>
+            </section>
             </article>
         </>
     );
