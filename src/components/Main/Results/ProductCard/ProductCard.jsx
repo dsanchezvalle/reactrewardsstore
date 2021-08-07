30e7f62b0cd1c8d4003503a772e15e8675680532
David Sanchez <dsanva2004@gmail.com>
2021-08-07 18:00:27 -0500

    feat:include icons buy and points needed to ProductCard component

--- src/components/Main/Results/ProductCard/ProductCard.jsx
+++ src/components/Main/Results/ProductCard/ProductCard.jsx
@@ -2,22 +2,35 @@
 import React from 'react'
 //Styles
 import './ProductCard.css'
+//Icons
+import { ReactComponent as BuyIcon } from "../../../../assets/images/buy.svg";
+import { ReactComponent as BuyIconHover } from "../../../../assets/images/buy-hover.svg";
+import { ReactComponent as Coin } from "../../../../assets/images/coin.svg";
 
 const ProductCard = ({name, category, imagePath, cost}) => {
-    let userPoints = 2200;
+    let userPoints = 500;
     //Check if current user can redeem a specific product 
     function userCanRedeem(points, productCost){
         return points>=productCost;
     }
-    
+
+    function pointsNeeded(points, productCost){
+        return cost - points;
+    }
+
     return(
         <>
             <article className="ProductCard">
-            <img className="ProductCard__Image" src={imagePath} alt={name} />
-            <section className="ProductCard__Info">
-                <p className="ProductCard__Category">{category} - {userCanRedeem(userPoints,cost)?<span style={{color: "green"}}>Yes</span>:<span style={{color: "red"}}>No</span>}</p>
-                <h3 className="ProductCard__Name">{name}</h3>
-            </section>
+                {userCanRedeem(userPoints,cost)?(
+                    <BuyIcon className="ProductCard__Icon"/>
+                ):(
+                    <div className="ProductCard__Icon ProductCard__PointsNeeded">{`You need ${pointsNeeded(userPoints,cost)}`}<Coin /></div>
+                )}
+                <img className="ProductCard__Image" src={imagePath} alt={name} />
+                <section className="ProductCard__Info">
+                    <p className="ProductCard__Category">{category}</p>
+                    <h3 className="ProductCard__Name">{name}</h3>
+                </section>
             </article>
         </>
     );
