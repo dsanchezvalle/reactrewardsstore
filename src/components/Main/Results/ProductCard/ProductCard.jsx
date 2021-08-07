370b3b3a6207752010443882b1d8179dd3dd622f
David Sanchez <dsanva2004@gmail.com>
2021-08-07 16:44:30 -0500

    feat:include cost and test function userCanRedeem

--- src/components/Main/Results/ProductCard/ProductCard.jsx
+++ src/components/Main/Results/ProductCard/ProductCard.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 //Styles
 import './ProductCard.css'
 
-const ProductCard = ({name, category, imagePath}) => {
+const ProductCard = ({name, category, imagePath, cost}) => {
+    let userPoints = 2200;
+    //Check if current user can redeem a specific product 
+    function userCanRedeem(points, productCost){
+        return points>=productCost;
+    }
+    
     return(
         <>
             <article className="ProductCard">
             <img className="ProductCard__Image" src={imagePath} alt={name} />
             <section className="ProductCard__Info">
-                <p className="ProductCard__Category">{category}</p>
+                <p className="ProductCard__Category">{category} - {userCanRedeem(userPoints,cost)?<span style={{color: "green"}}>Yes</span>:<span style={{color: "red"}}>No</span>}</p>
                 <h3 className="ProductCard__Name">{name}</h3>
             </section>
             </article>
