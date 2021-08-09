a0326bc3a30d57b8870ac91eadf52e89f89f64cb
David Sanchez <dsanva2004@gmail.com>
2021-08-09 09:18:19 -0500

    feat:add useEffect to fetch products from API

--- src/components/Main/TopBar/Filters/Filters.jsx
+++ src/components/Main/TopBar/Filters/Filters.jsx
@@ -4,22 +4,42 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../../../../contexts/AppContext';
 //Styles 
 import './Filters.css'
-//Test data
-import { products } from '../../../../utils/data';
 
 const Filters = () => {
-    const {setFilteredProducts} = useContext(AppContext);
+    const {setFilteredProducts, setErrorMessage} = useContext(AppContext);
     const [productList, setProductList] = useState([]);
     
     //1. Fetch productList from API and set it.
     useEffect(()=>{
-        setProductList(products);
+        if (productList.length===0){
+        async function getProducts(){
+            try{                
+                const fetchedData = 
+                await fetch("https://coding-challenge-api.aerolab.co/products", 
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk'
+                    },
+                    method: 'GET'
+                  });
+                  const itemList = await fetchedData.json();
+                  setProductList(itemList); 
+            }
+            catch (err){
+                setErrorMessage('Whoops! We got an error while bringing your products. Please, try again.');
+            }
+        }
+        getProducts();
+    }
+        //setProductList(products);
     //2. Apply filters to productList to produce filteredList
         const newFilteredProducts = productList; //This will be filtered
     //3. Set filteredList (context)
         setFilteredProducts(newFilteredProducts);
     }    
-    ,[productList]);    
+    ,[productList, setErrorMessage, setFilteredProducts]);    
 
     return(
         <>
