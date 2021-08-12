633c28e26552c7a0c0b6204a8cc657670d4e3f91
David Sanchez <dsanva2004@gmail.com>
2021-08-12 16:05:46 -0500

    feat:update Results component with ResultsGrid component and Navigation component

--- src/components/Main/Results/Results.jsx
+++ src/components/Main/Results/Results.jsx
@@ -3,59 +3,24 @@ import React, { useContext } from "react";
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
   } from "react-router-dom";
 //Styles
 import './Results.css'
 //Components
-import ProductCard from "./ProductCard/ProductCard";
+import Navigation from "./Navigation/Navigation";
 //Context
 import { AppContext } from "../../../contexts/AppContext";
+import ResultsGrid from "./ResultsGrid/ResultsGrid";
 
 const Results = () => {
     //Get filteredProducts and errorMessage from AppContext
-    const {filteredProducts, errorMessage, redeemHistory, setRedeemHistory, setErrorMessage} = useContext(AppContext);
-    
-    //It handles the redeem history request
-    function handleGetRedeemHistoryClick(){
-        
-        async function getRedeemHistory(){
-            try{
-                const fetchedData = await fetch('https://coding-challenge-api.aerolab.co/user/history',
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Accept': 'application/json',
-                            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTExNDkwNGQ5ZmMzODAwMjFmNjM4NDMiLCJpYXQiOjE2Mjg1MjI3NTZ9.9RRbOr2MKD1bfRKrqBzfeTf6NqH153GOgb0Wu0pDNQk'
-                        },
-                        method: 'GET'
-                    }
-                    );
-                const response = await fetchedData.json();
-                setRedeemHistory(response);
-            }
-            catch(err){
-                setErrorMessage('Whoops! We got an error requesting your redeem history. Please, try again.');
-            }
-        }
-        getRedeemHistory();
-    }
+    const {filteredProducts, errorMessage, redeemHistory} = useContext(AppContext);
 
     return(
         <>        
             <Router>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Store </Link>
-                        </li>
-                        <li>
-                            <Link to="/history" onClick={handleGetRedeemHistoryClick}>Redeem History</Link>
-                        </li>
-                    </ul>
-                </nav>
-
+                <Navigation />
                 <Switch>
                     <Route path="/history">
                         <>
@@ -63,19 +28,7 @@ const Results = () => {
                         {redeemHistory.length === 0 && <p>You have not redeemed products yet.</p>}
                         {redeemHistory.length !== 0 && errorMessage.length === 0 ? 
                             (
-                                <section className="ResultsGrid">
-                                    {redeemHistory.map(
-                                        product => 
-                                        <ProductCard
-                                        key={product._id+product.name}
-                                        productId={product._id}
-                                        name={product.name}
-                                        category={product.category} 
-                                        imagePath={product.img.hdUrl}
-                                        cost={product.cost}
-                                        />
-                                    )}
-                                </section>
+                                <ResultsGrid productList={redeemHistory} />                                
                             ):(
                                 <section className='ResultsError'>
                                     <p>{errorMessage}</p>
@@ -86,26 +39,14 @@ const Results = () => {
                     </Route>
 
                     <Route path="/">
-                    { errorMessage.length === 0 ?(
-                        <section className="ResultsGrid">
-                            {filteredProducts.map(
-                                product => 
-                                <ProductCard
-                                key={product._id+product.name}
-                                productId={product._id}
-                                name={product.name}
-                                category={product.category} 
-                                imagePath={product.img.hdUrl}
-                                cost={product.cost}
-                                />
-                            )}
-                        </section>
-                    ):(
-                        <section className='ResultsError'>
-                            <p>{errorMessage}</p>
-                        </ section>
-                    )
-                    }
+                        { errorMessage.length === 0 ?(
+                            <ResultsGrid productList={filteredProducts} /> 
+                        ):(
+                            <section className='ResultsError'>
+                                <p>{errorMessage}</p>
+                            </ section>
+                        )
+                        }
                     </Route>
                </Switch> 
             </Router>
