a6da7bc65bfc3f743516ce6a4a24f8aaf3e088e2
David Sanchez <dsanva2004@gmail.com>
2021-08-14 07:13:41 -0500

    feat:create LoadingSpinner component and its styles

---
+++ src/components/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+//Dependencies
+import React from 'react'
+//Styles
+import './LoadingSpinner.css'
+
+const LoadingSpinner = () => {
+    return(
+        <>
+            <div id="spinner"></div>
+        </>
+    );
+};
+
+export default LoadingSpinner;
