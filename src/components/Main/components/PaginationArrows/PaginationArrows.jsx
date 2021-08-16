5549b8ec9d8c40ec1ee20283a44a1a2a741a882b
David Sanchez <dsanva2004@gmail.com>
2021-08-16 18:53:56 -0500

    feat:add next and previous page navigation in PaginationArrows

--- src/components/Main/components/PaginationArrows/PaginationArrows.jsx
+++ src/components/Main/components/PaginationArrows/PaginationArrows.jsx
@@ -1,17 +1,26 @@
 //Dependencies
-import React from 'react';
+import React, { useContext } from 'react';
 //Styles
 import './PaginationArrows.css'
 //Images and Icons
 import { ReactComponent as ArrowLeft } from "../../../../assets/images/arrow-left.svg";
 import { ReactComponent as ArrowRight } from "../../../../assets/images/arrow-right.svg";
+//Context
+import { AppContext } from '../../../../contexts/AppContext';
 
 const PaginationArrows = () => {
+    //Context
+    const {showFilters, nextProductsPage, prevProductsPage, nextRedeemedPage, prevRedeemedPage } = useContext(AppContext);
+    
+    /* If showFilters is true then the Store is active (showing available products), 
+    if not the Redeem History is active. */
+    let storeIsActive = showFilters;
+    
     return(
         <>
         <p className="PaginationArrowsContainer">
-            <ArrowLeft />
-            <ArrowRight />
+            <ArrowLeft onClick={()=>{storeIsActive ? prevProductsPage() : prevRedeemedPage()}} />
+            <ArrowRight onClick={()=>{storeIsActive ? nextProductsPage() : nextRedeemedPage()}} />
         </p>
         </>
     );
