1753182ade4d09cbf65ef30db1644cf31b028a9e
David Sanchez <dsanva2004@gmail.com>
2021-08-16 18:40:15 -0500

    feat:create usePagination custom hook

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,32 @@
+//Dependencies
+import React, { useState } from "react";
+
+function usePagination(data, itemsPerPage) {
+    //States
+    const [currentPage, setCurrentPage] = useState(1);
+    //Calculate maximum page
+    const maxPage = Math.ceil(data.length / itemsPerPage);
+
+    function currentData() {
+        const begin = (currentPage - 1) * itemsPerPage;
+        const end = begin + itemsPerPage;
+        return data.slice(begin, end);
+    }
+
+    function next() {
+        setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage));
+    }
+
+    function prev() {
+        setCurrentPage((currentPage) => Math.max(currentPage - 1, 1));
+    }
+
+    function jump(page) {
+        const pageNumber = Math.max(1, page);
+        setCurrentPage((currentPage) => Math.min(pageNumber, maxPage));
+    }
+
+    return [next, prev, jump, currentData, currentPage, maxPage];
+}
+
+export default usePagination;
