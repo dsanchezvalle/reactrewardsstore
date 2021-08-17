74b6945db488d8ebe5abb43c80db8fc2356ab8d7
David Sanchez <dsanva2004@gmail.com>
2021-08-16 22:50:47 -0500

    feat:calculate start and end item of each batch for current page

--- src/components/Main/components/ProductsPerPage/ProductsPerPage.jsx
+++ src/components/Main/components/ProductsPerPage/ProductsPerPage.jsx
@@ -1,12 +1,53 @@
 //Dependencies
-import React from 'react'
+import React, { useContext } from 'react'
 //Styles
 import './ProductsPerPage.css'
+//Context
+import { AppContext } from '../../../../contexts/AppContext';
 
 const ProductsPerPage = () => {
+    //Context
+    const { 
+        showFilters, 
+        currentProductsPage, 
+        currentRedeemedPage, 
+        productsPerPage, 
+        redeemedPerPage,
+        maxProductsPage,
+        maxRedeemedPage,
+        totalProducts,
+        totalRedeemed  
+    } = useContext(AppContext);
+    
+    /* If showFilters is true then the Store is active (showing available products), 
+    if not the Redeem History is active. */
+    let storeIsActive = showFilters;
+    
+    function getStartItemInBatch(currentPage, itemsPerPage){
+        return currentPage === 1 ? 1 : (((currentPage - 1)*itemsPerPage)+1);
+    };
+    
+    function getEndItemInBatch(currentPage, itemsPerPage, maxPage, totalItems){
+        return currentPage === maxPage ? totalItems : currentPage * itemsPerPage;
+    };
+
+    let startItemInBatch = storeIsActive ? (
+        getStartItemInBatch(currentProductsPage, productsPerPage)
+    ):(
+        getStartItemInBatch(currentRedeemedPage, redeemedPerPage)
+    )
+
+    let endItemInBatch = storeIsActive ? (
+        getEndItemInBatch(currentProductsPage, productsPerPage, maxProductsPage, totalProducts)
+    ):(
+        getEndItemInBatch(currentRedeemedPage, redeemedPerPage, maxRedeemedPage, totalRedeemed)
+    );
+    
+    let totalItems = storeIsActive ? (totalProducts):(totalRedeemed);
+
     return (
         <>
-            <p className="ProductsPerPage">16 of 32 products</p>
+            <p className="ProductsPerPage">{startItemInBatch} - {endItemInBatch} of {totalItems} products</p>
         </>
     );
 }
