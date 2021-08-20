3df69eef956a6d144fc2e3407c11add42342310d
David Sanchez <dsanva2004@gmail.com>
2021-08-20 17:52:24 -0500

    fix:remove unused imports in Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,11 @@
 //Dependencies
 import React from "react";
-import { useContext } from "react";
 //Components
 import TopBar from "./TopBar/TopBar";
 import Results from "./Results/Results";
 import Footer from "./Footer/Footer";
 //Styles
 import './Main.css'
-//Context
-import { AppContext } from "../../contexts/AppContext";
 
 const Main = () => {
     return(
