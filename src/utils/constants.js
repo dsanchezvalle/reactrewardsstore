704a289ad6817922c0bfc48ab52f9b6640582091
David Sanchez <dsanva2004@gmail.com>
2021-08-20 15:36:25 -0500

    style:add comments to constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,14 @@
+//App URLs
 export const URLS = {
     publicPath: process.env.PUBLIC_URL
 };
 
+//Options to request points to the API
 export const getPointsOptions = [1000, 5000, 7500]; 
 
 /* Filters initial state 
 FilterPrice 0: has default sorting from API
-FilterCategory 0: includes all categories
+FilterCategory 0: includes all products categories
 */
 export const initialFilters = [
     {
