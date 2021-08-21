b994900b5f95f6f2441249ef57c03528a8fc2303
David Sanchez <dsanva2004@gmail.com>
2021-08-20 23:27:05 -0500

    feat:add points redemption messages

--- src/assets/lang/languages.js
+++ src/assets/lang/languages.js
@@ -1,8 +1,18 @@
 export const languages = {
     en:{
-        title:'Electronics'
+        title:'Electronics',
+        morePointsMsg:'Get more points...',
+        pointsSuccessTitle:'Enjoy your points!',
+        pointsSuccessMsg: function (redeemedPoints, newBalance){
+            return `You have redeemed ${redeemedPoints} points successfully. Now you have ${newBalance} points.`
+        }
     },
     es:{
-        title:'Electrónica'
+        title:'Electrónica',
+        morePointsMsg:'Pide más puntos...',
+        pointsSuccessTitle:'¡Disfruta tus puntos!',
+        pointsSuccessMsg: function (redeemedPoints, newBalance){
+            return `Has redimido ${redeemedPoints} puntos exitosamente. Ahora tienes ${newBalance} puntos.`
+        }        
     }
 }
